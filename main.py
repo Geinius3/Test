7a524dfc145604430be82709b2ebe52a9b5921e8
trino <trinow2019@gmail.com>
2024-03-10 23:46:29 +0200

    tgbot

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("Never gonna give you up!")
+import asyncio
+import random
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+rickroll = "Never gonna give you up!"
+print(rickroll)
+
+TOKEN_API = "6173306659:AAGv-jPLFQFOsMCdykqkZ7HH8O7OAAX4vrc"
+bot = Bot(TOKEN_API)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привіт, я бот-рандомайзер!'
+                         '\nПоки що я нічого не вмію, але це лише перший етап.'
+                         '\nСпробуй /help для отримання списку команд.')
+
+@dp.message(Command('help'))
+async def get_help(message: Message):
+    await message.answer("Список доступних команд: \n"
+                         "/start \n/help "
+                         "\nНажаль поки що немає інших команд(( ")
+
+@dp.message(F.text=='пасхалка')
+async def rickroll_message(message: Message):
+    await message.answer('Never gonna give you up!')
+    await message.answer('Never gonna let you down!')
+    await message.answer('Never gonna run around and desert you!')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
